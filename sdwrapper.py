14128e7b8381058714dd8eba5e5e46fb8c0b51e7
unknown <angohz@ymail.com>
2022-09-15 01:40:42 +0200

    some improvements, add script to evaluate impact of image size

--- sdwrapper.py
+++ sdwrapper.py
@@ -22,7 +22,21 @@ SETTINGS_TEST = {
 	"width" : 512,
 	"height" : 512,
 	"ddim_steps" : 30,
+	"n_samples" : 3
+}
+
+SETTINGS_EXPLORE = {
+	"width" : 512,
+	"height" : 512,
+	"ddim_steps" : 50,
 	"n_samples" : 5
+}
+
+SETTINGS_HQ = {
+	"width" : 512,
+	"height" : 512,
+	"ddim_steps" : 80,
+	"n_samples" : 1
 }
 
 SETTINGS_DEFAULT_PORTRAIT = {
@@ -45,27 +59,65 @@ def load_settings(settings):
 def get_seed():
 	return np.random.randint(1000000)
 	
-def run_prompts(name, promptfile="prompts_example", stylefile="styles_example", settings=SETTINGS_DEFAULT_PORTRAIT):
-	print (PROMPTSDIR)
-	print (promptfile)
-	with open(PROMPTSDIR+"/"+promptfile+".json", 'r') as promptsfile:
-		prompts = json.load(promptsfile)
+def _get_prompt(content, style):
+	return "\""+content+", "+style+"\""
+	
+def _init(name, contentfile, contentkeys, stylefile, stylekeys):
+	print ("get prompts from "+PROMPTSDIR)
+	print ("\t.../"+contentfile+".json")
+	print ("\t.../"+stylefile+".json")
+	with open(PROMPTSDIR+"/"+contentfile+".json", 'r') as contentfile:
+		contents = json.load(contentfile)
+		if contentkeys is not None:
+			contents = {key: contents[key] for key in contentkeys}
 	with open(PROMPTSDIR+"/"+stylefile+".json", 'r') as stylefile:
 		styles = json.load(stylefile)
-	
+		if stylekeys is not None:
+			styles = {key: styles[key] for key in stylekeys}
 	outdir = OUTDIR+"/"+name
-	width, height, ddim_steps, n_samples = load_settings(settings)		
-	seed = get_seed()
+	return contents, styles, outdir
 	
-	for promptkey in prompts:
-		prompt = prompts[promptkey]
-		#print (prompt)
+def _construct_full_command(prompt, width, height, ddim_steps, n_samples, seed, outdir):
+	return "python "+TXT2IMG_PATH+" --turbo --W "+str(width)+" --H "+str(height)+" --ddim_steps "+str(ddim_steps)+" --n_samples "+str(n_samples)+" --seed "+str(seed)+" --prompt "+prompt+" --outdir "+outdir
+
+def explore_imagesize(contentfile, contentkeys, stylefile, stylekeys, seed=None):
+	contents, styles, outdir = _init("explore_imagesize", contentfile, contentkeys, stylefile, stylekeys)
+	if seed is None:
+		seed = get_seed()
+	
+	for contentkey in contents:
 		for stylekey in styles:
-			#print (stylekey)
 			# construct command:
-			full_prompt = "\""+prompt+", "+styles[stylekey]+"\""
+			full_prompt = _get_prompt(contents[contentkey], styles[stylekey])
+			
+			for size in [128, 256, 384, 512, 768, 1024]:
+				full_command = _construct_full_command(full_prompt, size, size, 50, 5, seed, outdir)
+				#print (full_command)
+				# run stable diffusion:
+				subprocess.run(full_command)
+				
+				# move files into common output directory:
+				orig_path_ = outdir+"/"+"_".join(re.split(":| ", full_prompt[1:-1]))
+				orig_path = orig_path_[:150]
+				for file in os.listdir(orig_path):
+					fileparts = file.split('.')
+					file_seed = fileparts[0].split('_')[1]
+					os.replace(orig_path+"/"+file, outdir+"/"+contentkey+"_"+stylekey+"_"+file_seed+"_"+str(size)+"."+fileparts[-1])
+				os.rmdir(orig_path)
+
+
+def run_prompts(name, contentfile="contents_example", contentkeys=None, stylefile="styles_example", stylekeys=None, seed=None, settings=SETTINGS_DEFAULT_PORTRAIT):
+	contents, styles, outdir = _init(name, contentfile, contentkeys, stylefile, stylekeys)
+	width, height, ddim_steps, n_samples = load_settings(settings)
+	if seed is None:
+		seed = get_seed()
+	
+	for contentkey in contents:
+		for stylekey in styles:
+			# construct command:
+			full_prompt = _get_prompt(contents[contentkey], styles[stylekey])
 			#print (full_prompt)
-			full_command = "python "+TXT2IMG_PATH+" --turbo --W "+str(width)+" --H "+str(height)+" --ddim_steps "+str(ddim_steps)+" --n_samples "+str(n_samples)+" --seed "+str(seed)+" --prompt "+full_prompt+" --outdir "+outdir
+			full_command = _construct_full_command(full_prompt, width, height, ddim_steps, n_samples, seed, outdir)
 			#print (full_command)
 			# run stable diffusion:
 			subprocess.run(full_command)
@@ -75,12 +127,16 @@ def run_prompts(name, promptfile="prompts_example", stylefile="styles_example", 
 			orig_path = orig_path_[:150]
 			for file in os.listdir(orig_path):
 				fileparts = file.split('.')
-				newfilename = fileparts[0].split('_')[1]+"."+fileparts[-1]
-				os.replace(orig_path+"/"+file, outdir+"/"+promptkey+"_"+stylekey+"_"+newfilename)
+				file_seed = fileparts[0].split('_')[1]
+				os.replace(orig_path+"/"+file, outdir+"/"+contentkey+"_"+stylekey+"_"+file_seed+"."+fileparts[-1])
 			os.rmdir(orig_path)
 
 
 if __name__ == "__main__":
 	print ("requires \"conda activate ldm\"!")
+	
+	# run set of prompts:
 	run_prompts("debug", settings=SETTINGS_DEBUG)
 	
+	# explore impact of image size:
+	#explore_imagesize("contents_example", ["paris"], "styles_example", ["photo_lowquali"])
